43303476ed0b1f9a8b673bffea684d539fb1f6fb
GameDevRocky <159648130+GameDevRocky@users.noreply.github.com>
2025-09-16 16:17:02 -0400

    Events Page Completed, Create Event Page started

--- admin_app/admin/src/App.jsx
+++ admin_app/admin/src/App.jsx
@@ -3,16 +3,24 @@ import Login from "./pages/Login";
 import Dashboard from "./components/Dashboard";
 import Home from "./pages/Home";
 import Events from "./pages/Events";
+import CreateEvent from "./pages/CreateEvent";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* Login */}
         <Route path="/login" element={<Login />} />
+
+        {/* Dashboard wrapper */}
         <Route path="/" element={<Dashboard />}>
           <Route index element={<Home />} />
           <Route path="*" element={<Home />} />
-          <Route path="/events" element={<Events />} />
+
+          {/* Events + nested route */}
+          <Route path="events" element={<Events />}>
+            <Route path="create-event" element={<CreateEvent />} />
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
--- admin_app/admin/src/components/Dashboard.jsx
+++ admin_app/admin/src/components/Dashboard.jsx
@@ -1,12 +1,12 @@
-import { Outlet } from "react-router-dom";
 import Sidebar from "./SideBar";
 import Content from "./Content";
 import Navbar from "./Navbar";
+
 const Dashboard = () => {
   return (
-    <div className="w-full h-screen">
+    <div className="w-full h-screen flex flex-col">
       <Navbar />
-      <div className="flex h-full">
+      <div className="flex overflow-hidden">
         <Sidebar />
         <Content />
       </div>
