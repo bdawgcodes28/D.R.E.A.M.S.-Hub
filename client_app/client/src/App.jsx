f6ec5eb852f5456e220409846cfbfe434ca452d3
Elias Lopes <ealopes@aggies.ncat.edu>
2025-10-24 23:29:51 -0400

    implemented desktop and mobile navbar and some homepage components

--- client_app/client/src/App.jsx
+++ client_app/client/src/App.jsx
@@ -1,11 +1,21 @@
+import { BrowserRouter as Router } from "react-router-dom"
+import { Route, Routes } from "react-router-dom"
+
 import './App.css'
+import HomePage from "./pages/HomePage"
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <Router>
+      <Routes>
+        {/* home page | initial route */}
+        <Route path="/" element={<HomePage/>}/>
+
+        
+
+      </Routes>
+    </Router>
   )
 }
 
---
+++ client_app/client/src/hooks/index.js
@@ -0,0 +1,2 @@
+export { useIsMobile }              from './useIsMobile';
+export { default as useIsMobile }   from './useIsMobile';
